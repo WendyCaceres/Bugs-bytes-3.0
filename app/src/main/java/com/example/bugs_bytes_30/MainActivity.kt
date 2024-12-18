b57c2526ca1f5336b8686367f2abf13f0273fa85
Sebastian <sebasnocach@gmail.com>
2024-12-17 21:11:16 -0400

    auth

--- app/src/main/java/com/example/bugs_bytes_30/MainActivity.kt
+++ app/src/main/java/com/example/bugs_bytes_30/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.example.bugs_bytes_30
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.app.DatePickerDialog
+import android.content.Intent
+import android.widget.Button
+import android.widget.EditText
+import java.util.Calendar
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,8 +22,25 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val pantallaPrincipal = findViewById<Button>(R.id.button_siguiente)
-        pantallaPrincipal.setOnClickListener {
+        val dateEditText: EditText = findViewById(R.id.textInputEditTextDate)
+        setupDatePicker(dateEditText)
+    }
+    private fun setupDatePicker(editText: EditText) {
+        editText.setOnClickListener {
+            val calendar = Calendar.getInstance()
+            val year = calendar.get(Calendar.YEAR)
+            val month = calendar.get(Calendar.MONTH)
+            val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+            val datePickerDialog = DatePickerDialog(this, { _, selectedYear, selectedMonth, selectedDay ->
+                val formattedDate = String.format("%02d/%02d/%04d", selectedDay, selectedMonth + 1, selectedYear)
+                editText.setText(formattedDate)
+            }, year, month, day)
+
+            datePickerDialog.show()
+        }
+        val botonSiguiente = findViewById<Button>(R.id.button_siguiente)
+        botonSiguiente.setOnClickListener {
             val intent = Intent(this, Formulario2::class.java)
             startActivity(intent)
         }
